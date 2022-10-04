fa5fb36e32825a912d04db4696a641b371cc2ed6
ViktoriaFomenko <aktavia@gmail.com>
2022-10-04 17:23:33 +0300

    test

---
+++ index.js
@@ -0,0 +1,44 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
+
+// # Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+// node index.js --action="list"
+
+// # Отримуємо контакт по id
+// node index.js --action="get" --id=5
+
+// // # Додаємо контакт
+// node index.js --action="add" --name="Mango" --email="mango@gmail.com" --phone="322-22-22"
+
+// // # Видаляємо контакт
+// node index.js --action="remove" --id=3
